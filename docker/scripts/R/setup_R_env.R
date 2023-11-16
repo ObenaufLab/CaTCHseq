cf7b0f6c1809b5fa6bbdc7f56bf89f85066ba898
joerg.fallmann <joerg.fallmann@imp.ac.at>
2023-11-16 08:56:18 +0100

    gitignore update

--- docker/scripts/R/setup_R_env.R
+++ docker/scripts/R/setup_R_env.R
@@ -17,6 +17,7 @@ install.packages(packages[!grepl(pattern = "^BioConductor::", x = packages) & !g
 )
 
 # Install Bioconductor packages
+BiocManager::install(ask = FALSE)
 bc.packages <- packages[grepl(pattern = "^BioConductor::", x = packages)]
 bc.packages <- sub(pattern = "^BioConductor::", replacement = "", x = bc.packages)
 BiocManager::install(bc.packages)
@@ -25,5 +26,8 @@ BiocManager::install(bc.packages)
 re.packages <- packages[grepl(pattern = "^remotes::", x = packages)]
 re.packages <- sub(pattern = "^remotes::install_github", replacement = "", x = bc.packages)
 remotes::install_github(c(re.packages))
+
+# Install Seurat
+install.packages("Seurat")
 
 # Now we could install Signac
