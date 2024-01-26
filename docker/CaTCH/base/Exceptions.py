812cdb2126f9e0c173a0fab527a9f8da55533fde
joerg.fallmann <joerg.fallmann@imp.ac.at>
2024-01-26 12:42:30 +0100

    exception update

--- docker/CaTCH/base/Exceptions.py
+++ docker/CaTCH/base/Exceptions.py
@@ -8,13 +8,13 @@ class InvalidCellRecordException(Exception):
 
 class BarcodeMismatchException(Exception):
     def __init__(self, cell_barcode: str, new_barcode: str):
-        super.__init__(
+        super().__init__(
             f"The entry with the 10X barcode {new_barcode} cannot be added to the cell with the barcode {cell_barcode}"
         )
 
 
 class BarcodeLengthException(Exception):
     def __init__(self, bc1: str, bc2: str):
-        super.__init__(
+        super().__init__(
             f"Cannot calculate the Hamming distance for {bc1} and {bc2} since they have different lengths"
         )
