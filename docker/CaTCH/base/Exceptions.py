9e3a76207efe2ef5813e40c33248e7ade48c9e29
joerg.fallmann <joerg.fallmann@imp.ac.at>
2024-01-28 12:30:52 +0100

    print

--- docker/CaTCH/base/Exceptions.py
+++ docker/CaTCH/base/Exceptions.py
@@ -15,7 +15,7 @@ class BarcodeMismatchException(Exception):
 
 class BarcodeLengthException(Exception):
     def __init__(self, bc1: str, bc2: str):
-        print(f"bc1: {bc1}, bc2: {bc2}")
+        sys.exit(f"bc1: {bc1}, bc2: {bc2}")
         super().__init__(
             f"Cannot calculate the Hamming distance for {bc1} and {bc2} since they have different lengths"
         )
