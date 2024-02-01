f639aaba7a4f020158b89dce374ee3c0b7d4ae8b
joerg.fallmann <joerg.fallmann@imp.ac.at>
2024-02-01 15:05:05 +0100

    trim trailing EOF from barcodes

--- docker/scripts/python/countBarcodesInChunks.py
+++ docker/scripts/python/countBarcodesInChunks.py
@@ -4,40 +4,62 @@
     Runs the data preprocessing for each chunk separately.
 """
 
-import sys
 import argparse
 import os
+import sys
 from datetime import datetime
 from typing import List
-from CaTCH.base.singlecell.SingleCellDataReader import SingleCellDataReader
-from CaTCH.libraries.SingleCellLibrary import SingleCellLibrary
+
 import CaTCH.builtin.CaTCHBarcodeFilters as bcf
 import CaTCH.builtin.SingleCellRecordFilters as crf
+from CaTCH.base.singlecell.SingleCellDataReader import SingleCellDataReader
+from CaTCH.libraries.SingleCellLibrary import SingleCellLibrary
 
-
-FIXED_PAMS = [(21,24, "CCG"), (32,35, "CCN"), (45,48, "CCG")]
+FIXED_PAMS = [(21, 24, "CCG"), (32, 35, "CCN"), (45, 48, "CCG")]
 
 
 def loadValidCellIDs(filenames: List[str]) -> List[str]:
     cellIDs = []
     for f in filenames:
-        with(open(f, "rt")) as hInput:
+        with open(f, "rt") as hInput:
             next(hInput)
             for line in hInput:
                 chunks = line.split(",")
-                cellID = chunks[0].split("-")[0]
+                cellID = chunks[0].split("-")[0].rstrip()
                 cellIDs.append(cellID)
     return list(set(cellIDs))
 
 
 def main():
-    parser = argparse.ArgumentParser(description = "Preprocess single-cell CaTCH data")
+    parser = argparse.ArgumentParser(description="Preprocess single-cell CaTCH data")
 
-    parser.add_argument("--r1", type = str, required = True, nargs = '+', help = "path(s) to the files containing R1 reads (space-separated)")
-    parser.add_argument("--r2", type = str, required = True, nargs = '+', help = "path(s) to the files containing R2 reads (space-separated)")
-    parser.add_argument("--cellIDs", type = str, required = True, nargs = '+', help = "path(s) to the files containing the IDs of valid cells")
-    parser.add_argument("--counts", type = str, required = True, help = "path to the file to save the CaTCH barcode counts into")
-
+    parser.add_argument(
+        "--r1",
+        type=str,
+        required=True,
+        nargs="+",
+        help="path(s) to the files containing R1 reads (space-separated)",
+    )
+    parser.add_argument(
+        "--r2",
+        type=str,
+        required=True,
+        nargs="+",
+        help="path(s) to the files containing R2 reads (space-separated)",
+    )
+    parser.add_argument(
+        "--cellIDs",
+        type=str,
+        required=True,
+        nargs="+",
+        help="path(s) to the files containing the IDs of valid cells",
+    )
+    parser.add_argument(
+        "--counts",
+        type=str,
+        required=True,
+        help="path to the file to save the CaTCH barcode counts into",
+    )
 
     try:
         opts = parser.parse_args()
@@ -51,14 +73,15 @@ def main():
     cellIDs = loadValidCellIDs(opts.cellIDs)
     print(f"[{dt}] Loaded {len(cellIDs):,} cell IDs identified by CellRanger::count")
     # For performance reasons, organize the filters so that more computationally intensive
-    # filters come last. 
-    cellRecordFilters = [crf.CellRecordCaTCHBarcodeFilter(bcf.CaTCHNFilter(maxN = 0)),
-                         crf.CellRecordCaTCHBarcodeFilter(bcf.CaTCHHomopolymerFilter(maxPercent = 0.7)),
-                         crf.CellRecordCaTCHBarcodeFilter(bcf.CaTCHFixedElementsFilter(FIXED_PAMS))]
+    # filters come last.
+    cellRecordFilters = [
+        crf.CellRecordCaTCHBarcodeFilter(bcf.CaTCHNFilter(maxN=0)),
+        crf.CellRecordCaTCHBarcodeFilter(bcf.CaTCHHomopolymerFilter(maxPercent=0.7)),
+        crf.CellRecordCaTCHBarcodeFilter(bcf.CaTCHFixedElementsFilter(FIXED_PAMS)),
+    ]
 
-
-    reader = SingleCellDataReader(filenamesR1 = opts.r1, filenamesR2 = opts.r2)
-    lib = SingleCellLibrary(filters = cellRecordFilters, whitelist = cellIDs)
+    reader = SingleCellDataReader(filenamesR1=opts.r1, filenamesR2=opts.r2)
+    lib = SingleCellLibrary(filters=cellRecordFilters, whitelist=cellIDs)
     nProcessed = 0
     for sce in reader:
         lib.addCellRecord(sce)
@@ -66,7 +89,7 @@ def main():
         if nProcessed % 10_000 == 0:
             dt = datetime.now()
             print(f"  [{dt}] Processed {nProcessed:,} reads so far")
-    
+
     dt = datetime.now()
     print(f"[{dt}] Read {nProcessed:,} single cell entries")
     nRejected, stats = lib.getFilterStatistics()
@@ -77,7 +100,6 @@ def main():
     print(f"Found {lib.size:,} 10X cell IDs")
 
     lib.saveToJSON(opts.counts)
-
 
 
 if __name__ == "__main__":
